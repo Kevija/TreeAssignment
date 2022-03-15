1984d2425862ae3c34a8a213b268023c7acb08e8
ek <evija.kvante@inbox.lv>
2022-03-15 23:39:13 +0200

    Initial commit

---
+++ src/main/scala/TreeAssignment.scala
@@ -0,0 +1,51 @@
+import scala.io.StdIn.readLine
+
+object TreeAssignment extends  App {
+
+  //TODO ask person's name
+  //TODO ask for tree height
+  //TODO print a xmas tree (or another tree) by calling printTree function with the correct parameters
+  //tree height should be the one assigned
+  //simple version for height 3 would be
+  //  *
+  // ***
+  //*****
+
+  //for full points I would like to see the following
+  //if user enters name Valdis  and height 9
+  //then we should print
+  //        *
+  //       VVV
+  //      AAAAA
+  //     LLLLLLL
+  //    DDDDDDDDD
+  //   IIIIIIIIIII
+  //  SSSSSSSSSSSSS
+  // VVVVVVVVVVVVVVV
+  //AAAAAAAAAAAAAAAAA
+  // println(" "*10+"*"*5)
+  //let's consider maximum height 40 (so person's name letters could repeat many times)
+
+  val name = readLine("Hello! What is your name?")
+  val height = readLine("Enter you trees height:").toInt
+  val symbol = '*'
+  val maximumHeight = 40
+
+  def printTree(name: String, height: Int, symbol: Char = '*', maximumHeight: Int = 40): Unit = {
+
+    val name1 = name + name * (height / name.length)
+    if (height > maximumHeight) {
+      println("It is too high!")
+    } else {
+      println(" " * (height - 1) + s"$symbol")
+    }
+    for (i <- 2 to height) {
+      val a = name1(i - 2).toString.toUpperCase
+      println(" " * (height - i) + a * (i * 2 - 1))
+
+    }}
+  //for this exercise we are not going to adjust maximumHeight
+  //TODO print the tree HINT: check the last entry in Day7Strings
+  printTree(name, height, symbol, maximumHeight)
+
+}
