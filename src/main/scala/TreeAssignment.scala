5b241851d2298e36773c98b384afedf2ab6e6419
ek <evija.kvante@inbox.lv>
2022-03-19 16:51:13 +0200

    Some improvements

--- src/main/scala/TreeAssignment.scala
+++ src/main/scala/TreeAssignment.scala
@@ -29,23 +29,22 @@ object TreeAssignment extends  App {
   val name = readLine("Hello! What is your name?")
   val height = readLine("Enter you trees height:").toInt
   val symbol = '*'
-  val maximumHeight = 40
+  val maximumHeight:Int = 40
 
   def printTree(name: String, height: Int, symbol: Char = '*', maximumHeight: Int = 40): Unit = {
 
     val name1 = name + name * (height / name.length)
     if (height > maximumHeight) {
       println("It is too high!")
+
     } else {
       println(" " * (height - 1) + s"$symbol")
     }
-    for (i <- 2 to height) {
-      val a = name1(i - 2).toString.toUpperCase
-      println(" " * (height - i) + a * (i * 2 - 1))
+      for (i <- 2 to height) {
+        val a = name1(i - 2).toString.toUpperCase
+        println(" " * (height - i) + a * (i * 2 - 1))
+      }}
 
-    }}
-  //for this exercise we are not going to adjust maximumHeight
-  //TODO print the tree HINT: check the last entry in Day7Strings
-  printTree(name, height, symbol, maximumHeight)
+    printTree(name, height, symbol, maximumHeight)
 
 }
